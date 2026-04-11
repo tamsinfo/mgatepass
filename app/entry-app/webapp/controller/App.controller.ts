d72cb36f575cd92475edc37acfc36d6ce4c1ecd6
Vrishin Patel <vrishin_patel@outlook.com>
2026-04-11 09:00:00 +0530

    feat: scaffold entry-app UI5 frontend
    TypeScript UI5 app with ui5-tooling-transpile, OData V4 model bound to
    GatepassService, App wrapper to prevent re-render bug, and Hello World
    placeholder view

---
+++ app/entry-app/webapp/controller/App.controller.ts
@@ -0,0 +1,6 @@
+import BaseController from "mgatepass/entry/controller/BaseController";
+
+/**
+ * @namespace mgatepass.entry.controller
+ */
+export default class AppController extends BaseController {}
---
+++ app/entry-app/webapp/controller/BaseController.ts
@@ -0,0 +1,6 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace mgatepass.entry.controller
+ */
+export default class BaseController extends Controller {}
