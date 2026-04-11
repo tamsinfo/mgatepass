842454319f7353a590cf95d957b6ea78ca112090
Vrishin Patel <vrishin_patel@outlook.com>
2026-04-11 09:30:00 +0530

    feat: scaffold config-app UI5 frontend
    Admin configuration app bound to AdminService at /odata/v4/admin/,
    same structure as entry-app with Hello World placeholder view

---
+++ app/config-app/webapp/controller/App.controller.ts
@@ -0,0 +1,6 @@
+import BaseController from "mgatepass/config/controller/BaseController";
+
+/**
+ * @namespace mgatepass.config.controller
+ */
+export default class AppController extends BaseController {}
